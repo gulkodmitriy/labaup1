1e15fca50d5e9ecfc0c6308f6a147a5b1c7e18ae
gulkodmitriy <gulko.dmitriy1997@gmail.com>
2016-02-16 16:28:35 +0300

    Main

--- src/Chat/ComparerAmount.java
+++ src/Chat/ComparerAmount.java
@@ -2,9 +2,7 @@ package Chat;
 
 import java.util.Comparator;
 
-/**
- * Created by User on 16.02.2016.
- */
+
 public class ComparerAmount implements Comparator<Message> {
     @Override
     public int compare(Message a, Message b) {
